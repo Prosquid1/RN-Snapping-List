e6cbe944c55b20e4ea7a32085b24daf09edfeb80
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-09-30 17:57:42 +0100

    Rough draft for flexGrow

--- App.js
+++ App.js
@@ -32,11 +32,26 @@ const midPadding =
 const App = () => {
   const rows = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+  /**
+   * <View style={{flex:1,backgroundColor:'white'}}>
+    <View style={{justifyContent:'space-around'}}>
+      <View style={{height:50,alignSelf:'stretch',backgroundColor:'pink',margin:5}}/>
+      <View style={{height:50,alignSelf:'stretch',backgroundColor:'pink',marginHorizontal:5}}/>
+      <View style={{height:50,alignSelf:'stretch',backgroundColor:'pink',margin:5}}/>
+    </View>
+    <View style={{flex:1,alignItems:'center',justifyContent:'center',alignSelf:'stretch',backgroundColor:'blue',margin:5}}>
+      <Text style={{color:'white',fontWeight:'bold'}}>
+        View
+      </Text>
+    </View>
+  </View>
+   *
+   */
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.safeAreaView}>
+        <View style={styles.scrollViewContainer}>
           <ScrollView
             style={styles.scrollView}
             contentContainerStyle={styles.contentContainer}
@@ -47,37 +62,91 @@ const App = () => {
             decelerationRate={0}
             snapToAlignment="start"
             snapToInterval={profileViewWidth + blueCircleHorizontalMargin * 2}
-            scrollEventThrottle={36}>
+            scrollEventThrottle={36}
+            onScroll={(event) => {
+              var contentOffsetX = event.nativeEvent.contentOffset.x;
+              var contentOffsetY = event.nativeEvent.contentOffset.y;
+
+              var cellWidth = (DEVICE_WIDTH - 100).toFixed(2);
+              var cellHeight = (DEVICE_HEIGHT - 200).toFixed(2);
+
+              var cellIndex = Math.floor(contentOffsetX / cellWidth);
+
+              // Round to the next cell if the scrolling will stop over halfway to the next cell.
+              if (
+                contentOffsetX -
+                  Math.floor(contentOffsetX / cellWidth) * cellWidth >
+                cellWidth
+              ) {
+                cellIndex++;
+              }
+
+              // Adjust stopping point to exact beginning of cell.
+              contentOffsetX = cellIndex * cellWidth;
+              contentOffsetY = cellIndex * cellHeight;
+
+              event.nativeEvent.contentOffsetX = contentOffsetX;
+              event.nativeEvent.contentOffsetY = contentOffsetY;
+
+              // this.setState({contentOffsetX:contentOffsetX,contentOffsetY:contentOffsetY});
+              console.log('cellIndex:' + cellIndex);
+
+              console.log('contentOffsetX:' + contentOffsetX);
+              // contentOffset={{x:this.state.contentOffsetX,y:0}}
+            }}>
             {rows.map((item) => (
               <View style={styles.blueCircle}>
                 <Text style={styles.title}>{item}</Text>
               </View>
             ))}
           </ScrollView>
-          
-          <View style={styles.doucheBagSeparator} />
-        </>
-      </SafeAreaView>
-    </>
+        </View>
+        <View style={styles.detailsScrollView} />
+      </View>
+
+      <View style={styles.doucheBagSeparator} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  doucheBagSeparator: {
+  container: {
     flex: 1,
-    position: 'absolute',
+  },
+  safeAreaView: {
+    flex: 1,
+    marginTop: blueCircleHorizontalMargin,
+    backgroundColor: 'red',
+  },
+  scrollViewContainer: {
+    height: profileViewWidth + 30,
+    alignSelf: 'stretch',
+    backgroundColor: 'pink',
+  },
+  doucheBagSeparator: {
     height: DEVICE_HEIGHT,
     backgroundColor: 'red',
-    alignSelf: 'center',
+    position: 'absolute',
+    marginLeft: DEVICE_WIDTH / 2,
+    flex: 1,
     width: 1,
+  },
+  detailsScrollView: {
+    backgroundColor: 'pink',
+    flexGrow: 1,
+    alignSelf: 'stretch',
   },
   scrollView: {
     paddingStart: 0,
     paddingEnd: 0,
+    height: profileViewWidth + 30,
+    backgroundColor: 'blue',
   },
   contentContainer: {
     paddingHorizontal: midPadding,
+    alignItems: 'center',
   },
+
   blueCircle: {
     height: profileViewWidth,
     width: profileViewWidth,
